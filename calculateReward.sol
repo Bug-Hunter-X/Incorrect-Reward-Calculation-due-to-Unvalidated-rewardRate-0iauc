a480e3d21fefa4e08ec34d05af75b91f0c4be947
BugHunter-X <askzenai@gmail.com>
2025-01-03 10:52:06 +0100

    Add calculateReward.sol file

---
+++ calculateReward.sol
@@ -0,0 +1,7 @@
+function calculateReward(userAddress) {
+  // ... some code to fetch user's contribution ...
+
+  let reward = contribution * rewardRate; // rewardRate is a global variable
+
+  // ... some other code ...
+}
